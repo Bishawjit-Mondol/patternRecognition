e53ff39bcb3f92ba2876fa152cc13295a8dce4c0
robinNcode <msmrobin518@gmail.com>
2022-06-05 20:27:28 +0600

    hair length entropy not working

--- infoGain/solution.py
+++ infoGain/solution.py
@@ -1,14 +1,16 @@
 import pandas
 import math
+
+# GETTING THE DATA
 data = pandas.read_csv('prlab3dataset.csv')
+
 
 def entropy(maleCount, femaleCount, totalData):
     return -(maleCount / totalData) * (math.log2(maleCount / totalData)) - (femaleCount / totalData) * (
         math.log2(femaleCount / totalData))
 
-mainEntropy = 0
-maleCount = 0
-femaleCount = 0
+
+mainEntropy, maleCount, femaleCount = 0, 0, 0
 totalData = len(data)
 
 for datum in range(totalData):
@@ -21,18 +23,25 @@ mainEntropy = entropy(maleCount, femaleCount, totalData)
 
 print("Main Entropy: ", mainEntropy)
 
-hairLength = input("Enter the hair length <=: ")
+hairLength = int(input("Enter the hair length <=: "))
+yesCount, noCount, yesMaleCount, yesFemaleCount, noMaleCount, noFemaleCount = 0, 0, 0, 0, 0, 0
 
 for datum in range(totalData):
     if data.iloc[datum]['hair length'] <= hairLength:
+        yesCount += 1
         if data.iloc[datum]['class'] == 'm':
-            maleCount += 1
+            yesMaleCount += 1
         elif data.iloc[datum]['class'] == 'f':
-            femaleCount += 1
-hairLengthEntropy = entropy(maleCount, femaleCount, totalData)
-print("Hair Length Entropy: ", hairLengthEntropy)
+            yesFemaleCount += 1
+    else:
+        noCount += 1
+        if data.iloc[datum]['class'] == 'm':
+            noMaleCount += 1
+        elif data.iloc[datum]['class'] == 'f':
+            noFemaleCount += 1
 
+hairLengthYesEntropy = entropy(maleCount, femaleCount, yesCount)
+hairLengthNoEntropy = entropy(noMaleCount, noFemaleCount, noCount)
 
-
-
-
+print("Hair Length Yes Entropy: ", hairLengthYesEntropy)
+print("Hair Length No Entropy: ", hairLengthNoEntropy)
